f988c4d861cbc5665a1bbe83e3f5a43294a3c3a4
= <=>
2015-01-14 15:28:29 -0200

    Adicionada gema annotaten

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :integer          not null, primary key
+#  question   :text
+#  year       :datetime
+#  area       :string(255)
+#  subject    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#  hot        :boolean
+#  style      :string(255)
+#  subject_id :integer
+#
+
 class Question < ActiveRecord::Base
 
 	#Relacoes (Answers ainda deve ser implementado)
